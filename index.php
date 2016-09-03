201b64a51688ed5e3189fded87b55314f5c14791
William <wilcorrea@gmail.com>
2016-09-03 14:18:16 -0300

    #go

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$variavel = "Hello World!";
+
+$soma = 0 + 2;
+
+$mod = 10 % 2;
+
+?>
+<!DOCTYPE html>
+<html>
+  <?php
+
+  require_once "layout/head.php";
+
+  require_once "layout/body.php";
+
+  ?>
+</html>
